1f787ae2931bb7ede103ad8862b6ed27c35f4a8f
Yura338 <urazdor06@gmail.com>
2025-01-26 11:47:46 +0200

    initial commit

---
+++ pythonProject1/Пора_2.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.height = 178
+        print("Hi")
+
+
+vasya = Student("Hi")
+print(vasya.height)
+print("vasya.name")
+
