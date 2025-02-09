e352d4d0dd66cb0402642fdcb5b14763da1c962a
Yura338 <urazdor06@gmail.com>
2025-02-09 11:39:46 +0200

    para_6

---
+++ pythonProject1/Пара_6_5.py
@@ -0,0 +1,20 @@
+ch1 = int(input("Ведіть перше число"))
+ch2= int(input("Ведіть друге число"))
+ch3= int(input("Ведіть третє число"))
+
+
+def check(ch1, ch2, ch3):
+    if ch1 %5 == 0:
+        if ch2 %5 == 0:
+            if ch3 %5 == 0:
+                sp = [ch1, ch2, ch3]
+                sp.sort()
+                print(sp)
+    else:
+        raise TypeError("Не правильне число, всі мають ділитися на 5")
+
+try:
+    check(ch1, ch2, ch3)
+
+except TypeError:
+    print("Введено не правильні числа")
